c6edef7539ebbd9eb11ace34511491c7dfcaa753
CremonaLambert <cremonalambertmw@gmail.com>
2024-08-26 17:55:00 +0200

    Load and Haul animation

--- DesViz-main/convert_image.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-# Open the image file
-img = Image.open('resources/backdrop_rd.jpg')
-
-# Save it as a PNG file
-img.save('resources/backdrop_rd.png')
-
-print("Image converted successfully!")
